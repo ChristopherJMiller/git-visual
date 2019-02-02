33935e3030f77ae57120b67789c1c15226b7d0e0
Christopher Miller <admin@mctherealm.net>
2019-02-01 22:08:04 -0500

    Got most of commits working, git log is hard to figure out merges

--- index.js
+++ index.js
@@ -5,30 +5,41 @@ const port = 3000
 app.set('view engine', 'pug')
 app.use('/gitgraph/', express.static('node_modules/gitgraph.js/build/'))
 
-const gitlog = require('gitlog')
-
 app.get('/*', function (req, res) {
   if (req.path === "/favicon.ico" || req.path === "/chart.js" || req.path === "/node_modules/gitgraph.js/build/gitgraph.min.js")
     return
-  const options =
-    { repo: __dirname + '/..' + req.path
-    , number: 50,
-    all: true,
-    fields:
-      [ 'hash'
-      , 'abbrevHash'
-      , 'subject'
-      , 'authorName',
-      'parentHashes'
-      , 'authorDateRel'
-      ]
-    , execOptions:
-      { maxBuffer: 1000 * 1024
+
+
+  const git = require('simple-git')(__dirname + '/..' + req.path)
+  const gitlog = require('gitlog');
+
+  git.branch((err,branches) => {
+    let branchData = {};
+    for (let i = 0; i < branches.all.length; i++) {
+      let branchName = branches.all[i]
+
+      const options = {
+        repo: __dirname + '/..' + req.path,
+        number: 50,
+        branch: branchName !== 'master' ? branchName + ' --not master' : 'master',
+        fields: [
+          'abbrevHash',
+          'abbrevParentHashes',
+          'subject',
+          'authorName'
+        ],
+        execOptions: {
+          maxBuffer: 1000 * 1024
+        }
       }
-    };
-  console.log(__dirname + '..' + req.path)
-  let commits = gitlog(options);
-  res.render('index', { title: __dirname + req.path, data: commits })
+      let commits = gitlog(options)
+      if (commits.length > 0) {
+        branchData[branchName] = commits;
+      }
+    }
+    res.render('index', { title: __dirname + req.path, data: branchData })
+  })
+
 })
 
 app.listen(port)
